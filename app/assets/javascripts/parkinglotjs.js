49fd96ae3c91eabff6ba2c36eab80a15ae15c0f5
John McCrummen <jmccrummen17@gmail.com>
2016-10-17 14:41:09 -0600

    javascript started for spot info

--- app/assets/javascripts/parkinglotjs.js
+++ app/assets/javascripts/parkinglotjs.js
@@ -2,3 +2,30 @@ function spot(val){
 		console.log("SPOTting");
 		console.log("Width: " + val);
 }
+
+var SpotPopup = {
+	setup: function() {
+		var popupDiv = $('<div id="spotInfo"></div>');
+		popupDiv.hide().appendTo($('body'));
+		$(document).on('click', '#spots a', SpotPopup.getSpotInfo);
+	}
+	,getSpotInfo: function() {
+		$.ajax({type: 'GET',
+				url: $(this).attr('href'),
+				success: SpotPopup.showSpotInfo,
+				error: function(xhrObj, textStatus, exception) {alert('Error!');}
+	});
+	return(false);
+	}
+	,showSpotInfo: function(data, requestStatus, xhrObject) {
+		var oneFourth = Math.ceil($(window).width() / 4);
+		$('#spotInfo').css({'left': oneFourth, 'width': oneFourth, 'top': 250}).html(data).show();
+		$('#closeLink').click(SpotPopup.hideSpotInfo);
+		return(false);
+	}
+	,hideSpotInfo: function() {
+		$('#spotInfo').hide();
+		return(false);
+	}
+};
+$(SpotPopup.setup);
