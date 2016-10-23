cfa31abf0dcd87052d4152adeaae604d512e5783
mpizzol2 <mark.pizzolatto@gmail.com>
2016-10-23 12:56:07 -0600

    added user_login.feature

---
+++ features/step_definitions/user_login_steps.rb
@@ -0,0 +1,12 @@
+Given(/^I am on the login page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I enter my login credintials$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should go to my account page and see all my information$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
