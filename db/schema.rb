f3c3d689d5d2165a0bd78bf5247007878fad1ed6
John McCrummen <jmccrummen17@gmail.com>
2016-10-23 23:23:06 -0600

    file cleanup, migrated and seeded

--- db/schema.rb
+++ db/schema.rb
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20161023225314) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "LPN"
+    t.boolean  "is_handicap"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
