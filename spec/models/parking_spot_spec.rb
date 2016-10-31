ce5a91757a8da82787c1b5974a3a0093d7d07cd0
John McCrummen <jmccrummen17@gmail.com>
2016-10-30 20:36:52 -0600

    More rspec tests

--- spec/models/parking_spot_spec.rb
+++ spec/models/parking_spot_spec.rb
@@ -5,19 +5,21 @@ require 'rspec/its'
 RSpec.describe Parkingspot, type: :model do
 
   it { is_expected.to validate_presence_of(:status) }
-  it { is_expected.to validate_presence_of(:id) }
 
   describe 'Taken spot', 'Reserved spot' do
 	its(:occupying) {should_not == nil}
 
 
   	context 'User holding spot is logged in user' do
-  		let(:spot) {FactoryGirl.create :parkingspot}
-  		let(:occupying) {Type.find spot.occupying}
-  		let(:user) {FactoryGirl.create :user}
+  		let(:user) {FactoryGirl.build :user}
+  		let(:spot) {FactoryGirl.build :parkingspot, :occupying => user.id }
 
   		it 'recognize logged in user has the spot' do
   			expect(spot.occupying).to eq(user.id)
+  		end
+
+  		it 'user is marked as being parked' do
+  			expect(user.is_parked).to eq(true)
   		end
   	end
   end
@@ -27,12 +29,26 @@ RSpec.describe Parkingspot, type: :model do
 	its(:status) {should == 'open'}
 
 	context 'User is already holding a spot elsewhere' do
-		let(:user) {FactoryGirl.create :user}
+		let(:user) {FactoryGirl.build :user, :is_parked => true}
+		let(:spot) {FactoryGirl.build :parkingspot, :status => 'open' }
 
 		it 'cannot take multiple spots' do
-
+			expect(spot.occupying).to eq(nil)
 		end
 	end
+
+  end
+
+  describe 'Handicap spot' do
+  	context 'Spot is not empty' do
+  		let(:user) {FactoryGirl.build :user}
+  		let(:spot) {FactoryGirl.build :parkingspot, :status => 'taken', 
+  			:occupying => user.id, :spot_type => 'handicap'}
+
+  		it 'User holding spot needs to be handicap' do
+  			expect(user.is_handicap).to eq(true)
+  		end
+  	end
 
   end
 
