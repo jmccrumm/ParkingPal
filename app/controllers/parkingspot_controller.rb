09cf1ff84615e52d677c4b688439a599cd33f783
John McCrummen <jmccrummen17@gmail.com>
2016-11-14 10:15:00 -0700

    autofill email when messaging from spot

--- app/controllers/parkingspot_controller.rb
+++ app/controllers/parkingspot_controller.rb
@@ -3,6 +3,9 @@ class ParkingspotController < ApplicationController
 		id = params[:id] || 1
 		@spot = Parkingspot.find(id)
 		@user = current_user
+		if @spot.occupying != nil
+			session[:recipient] = User.find(@spot.occupying).email
+		end
 		render(partial: 'spot', object: @spot) if request.xhr?
 	end
 	
