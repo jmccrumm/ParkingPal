0eae70a513d75cb76160229aaf0d259bfc24de4d
Maegen Borden <mborden@uccs.edu>
2016-11-13 22:54:38 -0700

    Messaging & Stats Styling

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -7,10 +7,13 @@ class StatsController < ApplicationController
 		@taken = Parkingspot.where(status: 'taken').count('id');
 
 		@piechart = Gchart.pie(data: [@available, @reserved, @taken], 
-			labels: ['Available: ' + @available.to_s, 'Reserved: ' + @reserved.to_s, 'Taken: ' + @taken.to_s],
-			title: 'Lot Statistics (out of ' + @totalspots.to_s + ' spots)', theme: :keynote,
 
-			bg: 'a50e87');
+			labels: ['Available: ' + @available.to_s, 'Reserved: ' + @reserved.to_s, 'Taken: ' + @taken.to_s],
+			title: 'Lot Statistics (out of ' + @totalspots.to_s + ' spots)' , theme: :keynote,
+
+			bg: 'ffffff',
+			:graph_bg => 'd9cce0',
+			title_color: 'd9cce0');
 
 	end
 end
