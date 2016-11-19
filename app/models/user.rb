f1b99206ca960e1b5a5a4651111761996b3689cc
John McCrummen <jmccrummen17@gmail.com>
2016-11-19 16:28:58 -0700

    rspec for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,15 @@ class User < ActiveRecord::Base
 		return self.email
 	end
 
-	
+	validates :name, presence: true
+
+	validates :email, presence: true
+	validates :email, confirmation: true
+	validates :email, uniqueness: true
+
+	validates :password, presence: true
+	validates_length_of :password, :minimum => 6, :allow_nil => false
+
+	validates :LPN, presence: true
 	validates :LPN, format: {with: /\w{3}[-\s]?\w{3,4}/, message: 'Invalid License Plate'}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
+require 'rspec/its'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	it { is_expected.to validate_presence_of(:name) }
+	it { is_expected.to validate_presence_of(:LPN) }
+	#it { is_expected.to validate_format_of(:LPN) }
+	it { is_expected.to validate_presence_of(:email) }
+	it { is_expected.to validate_presence_of(:password) }
+
+	its(:password) {is_expected.to be_greater_than(5)} 
+
+	describe 'Non handicap user' do
+		context 'user is parked' do 
+			let(:user) {FactoryGirl.build :user, :is_parked => true, :is_handicap => false}
+			let(:spot) {FactoryGirl.build :parkingspot, :spot_type => 'normal', :occupying => user.id }
+
+			it 'spot cannot be handicap' do
+				expect(spot.spot_type).to eq('normal')
+			end
+
+		end
+	end
+
+
 end
